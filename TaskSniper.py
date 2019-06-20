b19d17207173bd00d00ad2bf3a2fb5772cf67415
Piter Pentester <user@localhost.localdomain>
2019-06-19 20:36:33 -0400

    MVP: CRUD tasks via class

--- TaskSniper.py
+++ TaskSniper.py
@@ -7,184 +7,109 @@ GREEN = {}
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	#print(message.chat.id)
 	bot.send_message(message.chat.id, "Hey! I can help you with your tasks!")
 	bot.send_message(message.chat.id, "My slogan is: One shot - one kill!")
 
+### Create class for CRUD tasks ###
+class TaskSniper:
+	### create task 
+	def new_task(self, message, DCT):
+		t = message.text.split(" ")
+		t = t[1:]
+		if t == []:
+			bot.send_message(message.chat.id, "Invalid input! Use /new_<priority> <your_task>")
+		else:
+			task = ''
+			for i in t:
+				task += i + " "
+			if DCT == {}:
+				DCT[message.chat.id] = [task]
+			else:
+				DCT[message.chat.id].append(task)
+			print("{}".format(DCT))
+			bot.send_message(message.chat.id, "Task '{}' created".format(task))
+	
+	### get tasks
+	def get_tasks(self, message, DCT):
+		if DCT != {}:
+			if message.chat.id in DCT.keys():
+				if DCT[message.chat.id] != []:
+					bot.send_message(message.chat.id, "Tasks: ")
+					for i in DCT[message.chat.id]:
+						bot.send_message(message.chat.id, i)
+				else:
+					bot.send_message(message.chat.id, "You haven't tasks")
+			else:
+					bot.send_message(message.chat.id, "You haven't tasks")
+		else:
+			bot.send_message(message.chat.id, "You haven't tasks")
+			
+	### delete task
+	def done(self, message, DCT):
+		t = message.text.split(" ")
+		t = t[1:]
+		if t == []:
+			bot.send_message(message.chat.id, "Invalid input! Use /done_<priority> <your_task> !")
+		else:
+			task = ''
+			for i in t:
+				task += i + " "
+			if DCT != {}:
+				if task in DCT[message.chat.id]:
+					DCT[message.chat.id].remove(task)
+					bot.send_message(message.chat.id, "Task '{}' deleted!".format(task))
+				else:
+					bot.send_message(message.chat.id, "You haven't task '{}'".format(task))
+			else:
+				bot.send_message(message.chat.id, "You haven't tasks!")
 
+### RED CRUD tasks ###
 @bot.message_handler(commands=['new_red'])
-def new_red(message):
-	#bot.send_message(message.chat.id, message.text)
-	t = message.text.split(" ")
-	t = t[1:]
-	if t == []:
-		print("Invalid input! Use /new_red <your_task>")
-		bot.send_message(message.chat.id, "Invalid input! Use /new_red <your_task>")
-	else:
-		task = ''
-		for i in t:
-			task += i + " "
-		if RED == {}:
-			RED[message.chat.id] = [task]
-		else:
-			RED[message.chat.id].append(task)
-		#RED[message.chat.id] = 
-		print("RED", RED)
-		bot.send_message(message.chat.id, "Red task '{}' created".format(task))
-
-
-@bot.message_handler(commands=['new_yellow'])
-def new_yellow(message):
-	#bot.send_message(message.chat.id, message.text)
-	t = message.text.split(" ")
-	t = t[1:]
-	if t == []:
-		print("Invalid input! Use /new_yellow <your_task>")
-		bot.send_message(message.chat.id, "Invalid input! Use /new_yellow <your_task>")
-	else:	
-		task = ''
-		for i in t:
-			task += i + " "
-		if YELLOW == {}:
-			YELLOW[message.chat.id] = [task]
-		else:
-			YELLOW[message.chat.id].append(task)
-		print("YELLOW", YELLOW)
-		bot.send_message(message.chat.id, "Yellow 'task {}' created".format(task))
-
-
-@bot.message_handler(commands=['new_green'])
-def new_green(message):
-	#bot.send_message(message.chat.id, message.text)
-	t = message.text.split(" ")
-	t = t[1:]
-	if t == []:
-		print("Invalid input! Use /new_green <your_task>")
-		bot.send_message(message.chat.id, "Invalid input! Use /new_green <your_task>")
-	else:
-		task = ''
-		for i in t:
-			task += i + " "
-		if GREEN == {}:
-			GREEN[message.chat.id] = [task]
-		else:
-			GREEN[message.chat.id].append(task)
-		print("GREEN", GREEN)
-		bot.send_message(message.chat.id, "Green task '{}' created".format(task))	
-
-
-### For get tasks ###
-
+def create(message):				
+	RED_CRUD = TaskSniper()
+	RED_CRUD.new_task(message, RED)
+	
 @bot.message_handler(commands=['red'])
-def new_red(message):
-	if RED != {}:
-		if message.chat.id in RED.keys():
-			if RED[message.chat.id] != []:
-				bot.send_message(message.chat.id, "RED tasks: ")
-				for i in RED[message.chat.id]:
-					bot.send_message(message.chat.id, i)
-			else:
-				bot.send_message(message.chat.id, "You haven't RED tasks")
-		else:
-				bot.send_message(message.chat.id, "You haven't RED tasks")
-	else:
-		bot.send_message(message.chat.id, "You haven't RED tasks")
-
-
-@bot.message_handler(commands=['yellow'])
-def new_yellow(message):
-	if YELLOW != {}:
-		if message.chat.id in YELLOW.keys():
-			if YELLOW[message.chat.id] != []:
-				bot.send_message(message.chat.id, "YELLOW tasks: ")
-				for i in YELLOW[message.chat.id]:
-					bot.send_message(message.chat.id, i)
-			else:
-				bot.send_message(message.chat.id, "You haven't YELLOW tasks")
-		else:
-			bot.send_message(message.chat.id, "You haven't YELLOW tasks")
-	else:
-		bot.send_message(message.chat.id, "You haven't YELLOW tasks")
-
-
-@bot.message_handler(commands=['green'])
-def new_green(message):
-	if GREEN != {}:
-		if message.chat.id in RED.keys():
-			if GREEN[message.chat.id] != []:
-				bot.send_message(message.chat.id, "GREEN tasks: ")
-				for i in GREEN[message.chat.id]:
-					bot.send_message(message.chat.id, i)
-			else:
-				bot.send_message(message.chat.id, "You haven't GREEN tasks")
-		else:
-			bot.send_message(message.chat.id, "You haven't GREEN tasks")
-	else:
-		bot.send_message(message.chat.id, "You haven't GREEN tasks")
-
-
-### for deleting tasks ###	
-
+def get(message):				
+	RED_CRUD = TaskSniper()
+	RED_CRUD.get_tasks(message, RED)
 
 @bot.message_handler(commands=['done_red'])
-def done_red(message):
-	t = message.text.split(" ")
-	t = t[1:]
-	if t == []:
-		bot.send_message(message.chat.id, "Invalid input! Use /done_red <your_task> !")
-	else:
-		task = ''
-		for i in t:
-			task += i + " "
-		if RED != {}:
-			if task in RED[message.chat.id]:
-				RED[message.chat.id].remove(task)
-				bot.send_message(message.chat.id, "RED task '{}' deleted!".format(task))
-			else:
-				bot.send_message(message.chat.id, "You haven't RED task '{}'".format(task))
-		else:
-			bot.send_message(message.chat.id, "You haven't RED tasks!")
+def delete(message):				
+	RED_CRUD = TaskSniper()
+	RED_CRUD.done(message, RED)
 
+### YELLOW CRUD tasks ###
+@bot.message_handler(commands=['new_yellow'])
+def create(message):				
+	YELLOW_CRUD = TaskSniper()
+	YELLOW_CRUD.new_task(message, YELLOW)
+	
+@bot.message_handler(commands=['yellow'])
+def get(message):				
+	YELLOW_CRUD = TaskSniper()
+	YELLOW_CRUD.get_tasks(message, YELLOW)
 
 @bot.message_handler(commands=['done_yellow'])
-def done_yellow(message):
-	t = message.text.split(" ")
-	t = t[1:]
-	if t == []:
-		bot.send_message(message.chat.id, "Invalid input! Use /done_yellow <your_task> !")
-	else:
-		task = ''
-		for i in t:
-			task += i + " "
-		if YELLOW != {}:
-			if task in YELLOW[message.chat.id]:
-				YELLOW[message.chat.id].remove(task)
-				bot.send_message(message.chat.id, "YELLOW task '{}' deleted!".format(task))
-			else:
-				bot.send_message(message.chat.id, "You haven't YELLOW task '{}'".format(task))
-		else:
-			bot.send_message(message.chat.id, "You haven't YELLOW tasks!")
-
+def delete(message):				
+	YELLOW_CRUD = TaskSniper()
+	YELLOW_CRUD.done(message, YELLOW)
+	
+### GREEN CRUD tasks ###
+@bot.message_handler(commands=['new_green'])
+def create(message):				
+	GREEN_CRUD = TaskSniper()
+	GREEN_CRUD.new_task(message, GREEN)
+	
+@bot.message_handler(commands=['green'])
+def get(message):				
+	GREEN_CRUD = TaskSniper()
+	GREEN_CRUD.get_tasks(message, GREEN)
 
 @bot.message_handler(commands=['done_green'])
-def done_green(message):
-	t = message.text.split(" ")
-	t = t[1:]
-	if t == []:
-		bot.send_message(message.chat.id, "Invalid input! Use /done_green <your_task> !")
-	else:
-		task = ''
-		for i in t:
-			task += i + " "
-		if GREEN != {}:
-			if task in GREEN[message.chat.id]:
-				GREEN[message.chat.id].remove(task)
-				bot.send_message(message.chat.id, "GREEN task '{}' deleted!".format(task))
-			else:
-				bot.send_message(message.chat.id, "You haven't GREEN task '{}'".format(task))
-		else:
-			bot.send_message(message.chat.id, "You haven't GREEN tasks!")
-
+def delete(message):				
+	GREEN_CRUD = TaskSniper()
+	GREEN_CRUD.done(message, GREEN)
 
 bot.polling()
 
